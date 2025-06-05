83f1963db02be55aaa865d608e974e3299a941f6
SabihSK <sabih.sk1@gmail.com>
2025-06-05 15:52:08 +0500

    add CORS in backend

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Yields:
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import router as api_router
 from core.db import Base, engine
@@ -24,5 +25,13 @@ async def lifespan(_app: FastAPI):  # renamed from app to _app
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <-- Your React frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
